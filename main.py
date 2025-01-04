335fbd928e9036039d3a3d36412c052262c2f2a0
riceju1ce <catloverglobal@gmail.com>
2025-01-05 02:17:17 +1030

    update to main.py
    fixed the toolbar search

--- main.py
+++ main.py
@@ -20,8 +20,7 @@ class LilVinnyBrowser(QMainWindow):
 
         self.url_bar = QLineEdit()
         self.url_bar.setPlaceholderText("Insert URL here for mommy...")
-        navbar.addWidget(self.url_bar)
-
+        self.url_bar.returnPressed.connect(self.navigate_to_url)
 
         back_button = QPushButton("Back")
         forward_button = QPushButton("Forward")
@@ -30,11 +29,20 @@ class LilVinnyBrowser(QMainWindow):
         back_button.clicked.connect(self.browser.back)
         forward_button.clicked.connect(self.browser.forward)
         refresh_button.clicked.connect(self.browser.reload)
+        self.browser.urlChanged.connect(self.update_url)
 
-        navbar.addWidget(self.url_bar)
+        #add widgets to the toolbar
         navbar.addWidget(back_button)
         navbar.addWidget(forward_button)
         navbar.addWidget(refresh_button)
+        navbar.addWidget(self.url_bar)
+
+    def navigate_to_url(self):
+        url = self.url_bar.text()
+        self.browser.setUrl(QUrl(url))
+
+    def update_url(self):
+        self.url_bar.setText(self.browser.url().toString())
 
 def main():
     app = QApplication(sys.argv)
